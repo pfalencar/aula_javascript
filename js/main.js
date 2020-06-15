044c87e14a1ef995a3eeffecbe7bf596bd7d5cfc
pfalencar <patricia.alencar@etec.sp.gov.br>
2020-06-15 11:00:30 -0300

    conexao arquivo js com arquivo html

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var nome = "Patricia Fernandes";
+var idade = "35";
+var idade2 = "10";
+var frase = "São Paulo é o melhor time do mundo!";
+var n1 = 5;
+var n2 = 3;
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(n1 * n2);
+/*
+console.log(frase);
+console.log(frase.replace("São Paulo", "Brasil"));
+alert (frase.replace("São Paulo", "Brasil"));
+*/
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
