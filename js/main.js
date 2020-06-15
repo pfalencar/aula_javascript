5f849528d3f4f4d38824aa8a5f10aceb80493029
pfalencar <patricia.alencar@etec.sp.gov.br>
2020-06-15 11:24:56 -0300

    Array e Dicionário - objeto JSON

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+//Lista de Dicionário
+var frutas = [{nome: "maçã", cor: "vermelha"},
+              {nome: "uva", cor: "roxa"}];
+console.log(frutas);
+alert (frutas[0].cor);
+
+/*
+//Dicionário
+var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta.nome);
+alert (fruta.cor);
+*/
+/*
+var lista = ["maçã", "pera", "laranja"];
+console.log(lista);
+//lista.pop();
+//lista.push("uva");
+//console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+
+console.log(lista[0]);
+console.log(lista.toString()[0]); 
+console.log(lista.toString());
+console.log(lista.join(" *-* "));
+console.log(lista.join(""));
+*/
+
+/*
 var nome = "Patricia Fernandes";
 var idade = "35";
 var idade2 = "10";
@@ -9,10 +38,11 @@ var n2 = 3;
 console.log(nome);
 console.log(idade + idade2);
 console.log(n1 * n2);
-/*
+
 console.log(frase);
 console.log(frase.replace("São Paulo", "Brasil"));
 alert (frase.replace("São Paulo", "Brasil"));
-*/
+
 console.log(frase.toUpperCase());
 console.log(frase.toLowerCase());
+*/
